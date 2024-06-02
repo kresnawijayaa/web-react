994fd1be91a2829a0c9bd3c65408a04fac13cf39
kresna <kresna.wijaya@indomaret.co.id>
2024-06-02 19:49:38 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Content from './pages/Content';
 import Contact from './pages/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './index.css';
 
 function App() {
   return (
